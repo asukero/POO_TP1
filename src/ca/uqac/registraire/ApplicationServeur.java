6333d9c62050845d208afc0a5757e5897d39742e
Asukero <thomas.fossati59@gmail.com>
2017-02-04 19:35:48 -0500

    compilation, creation and loading done.

--- src/ca/uqac/registraire/ApplicationServeur.java
+++ src/ca/uqac/registraire/ApplicationServeur.java
@@ -1,7 +1,14 @@
 package ca.uqac.registraire;
 
+import javax.tools.Diagnostic;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
@@ -9,12 +16,16 @@ import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class ApplicationServeur {
 
@@ -28,6 +39,8 @@ public class ApplicationServeur {
     private HashMap<String, Object> objectsCreated = new HashMap<>();
 
     private LinkedList<Class> classList = new LinkedList<>();
+
+    private ClassLoader classLoader;
 
     /**
      * prend le numéro de port, crée un SocketServer sur le port
@@ -79,14 +92,19 @@ public class ApplicationServeur {
     public void traiteCommande(Commande uneCommande) {
         switch (uneCommande.getCommandType()) {
             case COMPILATION:
-                traiterCompilation(sourcePath);
+                traiterCompilation(uneCommande.getAttributes().get("chemin_relatif_source"), uneCommande.getAttributes().get("chemin_relatif_classe"));
                 break;
             case CHARGEMENT:
                 traiterChargement(uneCommande.getAttributes().get("nom_qualifié_de_classe"));
                 break;
             case CREATION:
-                Class classACreer = getClass();
-                traiterCreation(classACreer, uneCommande.getAttributes().get("identificateur"));
+                try {
+                    Class classACreer = classList.stream().filter(c -> c.getName().equals(uneCommande.getAttributes().get("nom_de_classe"))).findFirst().get();
+                    traiterCreation(classACreer, uneCommande.getAttributes().get("identificateur"));
+                } catch (NoSuchElementException ex) {
+                    objectToSendBack = "ERREUR: La classe " + uneCommande.getAttributes().get("nom_de_classe") + " n'a pas été trouvé sur le serveur";
+                    sortieWriter.println(objectToSendBack);
+                }
                 break;
             case ECRITURE:
                 Object objectToWrite = objectsCreated.get(uneCommande.getAttributes().get("identificateur"));
@@ -94,8 +112,8 @@ public class ApplicationServeur {
                 if (objectToWrite != null) {
                     traiterEcriture(objectToWrite, uneCommande.getAttributes().get("nom_attribut"), uneCommande.getAttributes().get("valeur"));
                 } else {
-                    sortieWriter.println("ERREUR: L'objet " + uneCommande.getAttributes().get("identificateur") + " n'a pas été trouvé sur le serveur");
                     objectToSendBack = "ERREUR: L'objet " + uneCommande.getAttributes().get("identificateur") + " n'a pas été trouvé sur le serveur";
+                    sortieWriter.println(objectToSendBack);
                 }
                 break;
             case LECTURE:
@@ -104,8 +122,9 @@ public class ApplicationServeur {
                 if (objectToRead != null) {
                     traiterLecture(objectToRead, uneCommande.getAttributes().get("nom_qualifié_de_classe"));
                 } else {
-                    sortieWriter.println("ERREUR: L'objet " + uneCommande.getAttributes().get("identificateur") + " n'a pas été trouvé sur le serveur");
                     objectToSendBack = "ERREUR: L'objet " + uneCommande.getAttributes().get("identificateur") + " n'a pas été trouvé sur le serveur";
+                    sortieWriter.println(objectToSendBack);
+
                 }
                 break;
             case FONCTION:
@@ -126,10 +145,14 @@ public class ApplicationServeur {
                     }
                     traiterAppel(objectToCall, uneCommande.getAttributes().get("nom_fonction"), types.toArray(new String[0]), valeurs.toArray(new String[0]));
                 } else {
-                    sortieWriter.println("ERREUR: L'objet " + uneCommande.getAttributes().get("identificateur") + " n'a pas été trouvé sur le serveur");
                     objectToSendBack = "ERREUR: L'objet " + uneCommande.getAttributes().get("identificateur") + " n'a pas été trouvé sur le serveur";
+                    sortieWriter.println(objectToSendBack);
+
                 }
                 break;
+            default:
+                objectToSendBack = "Erreur: Le type de la commande est inconnue.";
+                sortieWriter.println(objectToSendBack);
         }
     }
 
@@ -146,7 +169,7 @@ public class ApplicationServeur {
      * s’est faite correctement.     
      */
     public void traiterEcriture(Object pointeurObjet, String attribut, Object valeur) {
-        System.out.println("ecriture");
+        
     }
 
     /**
@@ -154,7 +177,15 @@ public class ApplicationServeur {
      * s’est faite correctement.     
      */
     public void traiterCreation(Class classeDeLobjet, String identificateur) {
-        System.out.println("creation");
+        try{
+            Object createdObject = classeDeLobjet.newInstance();
+            objectsCreated.put(identificateur, createdObject);
+            objectToSendBack = "OK: L'objet " + identificateur + " de la classe " + classeDeLobjet.getName() + " a été correctement crée.";
+            sortieWriter.println(objectToSendBack);
+        }catch (InstantiationException | IllegalAccessException ex){
+            objectToSendBack = "Erreur: Un problème est survenu lors de l'instanciation de la classe.";
+            sortieWriter.println(objectToSendBack);
+        }
     }
 
     /**
@@ -162,7 +193,16 @@ public class ApplicationServeur {
      * s’est faite correctement.     
      */
     public void traiterChargement(String nomQualifie) {
-        System.out.println("chargement");
+        try {
+            Class classe = classLoader.loadClass(nomQualifie);
+            classList.add(classe);
+            objectToSendBack = "OK: La classe " + nomQualifie + "a été correctement chargée.";
+            sortieWriter.println(objectToSendBack);
+        } catch (ClassNotFoundException ex) {
+            objectToSendBack = "Erreur: La classe " + nomQualifie + " n'a pas été trouvée.";
+            sortieWriter.println(objectToSendBack);
+        }
+
     }
 
     /**
@@ -170,8 +210,70 @@ public class ApplicationServeur {
      * que la compilation s’est faite correctement. Le fichier source est donné par son chemin
      * relatif par rapport au chemin des fichiers sources.     
      */
-    public void traiterCompilation(String cheminRelatifFichierSource) {
-        System.out.println("compilation");
+    public void traiterCompilation(String cheminFichierSource, String cheminFichierClass) {
+
+        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
+
+
+        String[] cheminsFichiers = cheminFichierSource.split(",");
+        LinkedList<File> classesToCompile = new LinkedList<>();
+
+        for (String cheminFichier : cheminsFichiers) {
+            File file = new File(cheminFichier);
+            if (file.exists()) {
+                classesToCompile.add(file);
+            } else {
+                sortieWriter.println("Attention: le fichier " + cheminFichier + " n'a pas été trouvé");
+            }
+
+        }
+
+        if (!Files.isDirectory(Paths.get(cheminFichierClass))) {
+            sortieWriter.println("Attention: le dossier " + cheminFichierClass + " n'a pas été trouvé, utilisation du chemin par défaut du serveur");
+            cheminFichierClass = classPath;
+        }
+
+        // This sets up the class path that the compiler will use.
+        // I've added the .jar file that contains the DoStuff interface within in it...
+        List<String> optionList = new ArrayList<>();
+        optionList.add("-g");
+        optionList.add("-d");
+        optionList.add("./" + cheminFichierClass);
+
+
+        Iterable<? extends JavaFileObject> compilationUnit
+                = fileManager.getJavaFileObjectsFromFiles(classesToCompile);
+        JavaCompiler.CompilationTask task = compiler.getTask(
+                null,
+                fileManager,
+                diagnostics,
+                optionList,
+                null,
+                compilationUnit);
+
+        if (task.call()) {
+            /** Load and execute *************************************************************************************************/
+            objectToSendBack = "Les fichiers ";
+            for (File file : classesToCompile) {
+                objectToSendBack = objectToSendBack + file.getPath() + " ";
+            }
+            objectToSendBack = objectToSendBack + "ont été correctement compilés.";
+            sortieWriter.println(objectToSendBack);
+            /************************************************************************************************* Load and execute **/
+        } else {
+            for (Diagnostic<? extends JavaFileObject> diagnostic : diagnostics.getDiagnostics()) {
+                sortieWriter.println("Erreur à la ligne" + diagnostic.getLineNumber() + " de " + diagnostic.getSource().toUri());
+            }
+        }
+
+        try {
+            fileManager.close();
+        } catch (IOException e) {
+            sortieWriter.println("Erreur: Les fichiers sources ne se sont pas fermés correctement");
+        }
+
     }
 
     /**
@@ -200,6 +302,7 @@ public class ApplicationServeur {
                 throw new IllegalArgumentException("Veuillez indiquer 4 arguments");
             } else {
                 ApplicationServeur applicationServeur = new ApplicationServeur(new Integer(args[0]));
+
                 applicationServeur.sortieWriter = new PrintWriter(args[3]);
                 applicationServeur.sortieWriter.println("Le serveur est prêt");
                 applicationServeur.sourcePath = args[1];
@@ -212,6 +315,8 @@ public class ApplicationServeur {
                 if (!Files.isDirectory(Paths.get(applicationServeur.classPath))) {
                     throw new IOException("Le chemin classes n'est pas un dossier ou n'existe pas");
                 }
+
+                applicationServeur.classLoader = new URLClassLoader(new URL[]{new File(applicationServeur.sourcePath).toURI().toURL()});
 
                 Runtime.getRuntime().addShutdownHook(new Thread() {
                     @Override
@@ -229,7 +334,7 @@ public class ApplicationServeur {
 
             }
         } catch (Exception ex) {
-            System.out.println(ex.toString());
+            System.err.println(ex.toString());
             System.out.println("Usage:\n" +
                     "\tjava -jar ApplicationServer.jar [port number] [source folder] [class folder] [output filename]\n" +
                     "\tex: java -jar ApplicationServer.jar 4242 ./sources/ ./classes sortieServeur.txt");
